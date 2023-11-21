f35c847204a2fe97c133c39bf54c6b8b44a9904f
Arjan Tijms <arjan.tijms@omnifish.ee>
2023-11-21 18:35:09 +0100

    Policy replacement as per #99
    Signed-off-by: Arjan Tijms <arjan.tijms@omnifish.ee>

---
+++ api/src/main/java/jakarta/security/jacc/Policy.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2023 Contributors to Eclipse Foundation. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.security.jacc;
+
+import static java.util.Collections.emptySet;
+
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.security.Principal;
+import java.util.Set;
+import javax.security.auth.Subject;
+
+/**
+ * A Policy object is responsible for determining whether a caller principal (including the unauthenticated one) has permission
+ * to perform a security-sensitive operation.
+ *
+ * <p>
+ * A Policy uses the {@link Subject} as a holder for the caller principal. A Subject, being a "bag of principals" does not
+ * specify which of the potentially many principals represents the caller principal. As a low level artifact (aimed at
+ * Jakarta EE runtime interaction with the security system) this allows for an amount of runtime specific behaviour and optimisations.
+ *
+ * <p>
+ * Policies typically, but not necessarily, make use of the Permission instances hold by the {@link PolicyConfiguration} instance for
+ * a given policy context. In a Jakarta EE Servlet environment these contain the transformed security constraints as expressed by XML
+ * in web.xml, via annotations, or which are programmatically set using the Jakarta Servlet APIs.
+ *
+ * <p>
+ * <b>NOTE: DRAFT API. SUBJECT TO CHANGE</b>
+ *
+ * @author Arjan Tijms
+ */
+public interface Policy {
+
+    /**
+     * This method checks whether the permission represented by the @{permissionToBeChecked} parameter is granted to
+     * the caller principal within the @{subject} parameter.
+     *
+     * @param permissionToBeChecked the permission this policy is going to check
+     * @param subject holder of the (obscured) caller principal
+     * @return true if the caller principal has the requested permission, false otherwise
+     */
+    boolean implies(Permission permissionToBeChecked, Subject subject);
+
+    /**
+     * Returns a collection of at least all declared permissions associated with the caller principal
+     * contained in the @{subject} parameter.
+     *
+     * <p>
+     * Policies can represent remote authorization systems which may not be able to provide all permissions, and
+     * there for this method cannot guarantee all permissions are indeed returned. The policy should however
+     * return at least all permissions which are declared or set within a Jakarta EE application. Examples of such permissions
+     * are the permissions transformed from the Jakarta Servlet security constraints expression in @{web.xml}, via annotations
+     * or programmatically using the Jakarta Servlet API.
+     *
+     * @param subject holder of the (obscured) caller principal
+     * @return a collection of permissions associated with the caller principal
+     */
+    PermissionCollection getPermissionCollection(Subject subject);
+
+    /**
+     * This method checks whether the permission represented by the @{permissionToBeChecked} parameter is granted to
+     * the anonymous (unauthenticated) caller principal.
+     *
+     * @param permissionToBeChecked the permission this policy is going to check
+     * @return true if the anonymous caller principal has the requested permission, false otherwise
+     */
+    default boolean implies(Permission permissionToBeChecked) {
+        return implies(permissionToBeChecked, emptySet());
+    }
+
+    /**
+     * Returns a collection of at least all declared permissions associated with the caller principal
+     * contained in the set of principals being passed in.
+     *
+     * <p>
+     * Policies can represent remote authorization systems which may not be able to provide all permissions, and
+     * there for this method cannot guarantee all permissions are indeed returned. The policy should however
+     * return at least all permissions which are declared or set within a Jakarta EE application. Examples of such permissions
+     * are the permissions transformed from the Jakarta Servlet security constraints expression in @{web.xml}, via annotations
+     * or programmatically using the Jakarta Servlet API.
+     *
+     * @param permissionToBeChecked the permission this policy is going to check
+     * @param principals collection containing the (obscured) caller principal
+     * @return a collection of permissions associated with the caller principal
+     */
+    default boolean implies(Permission permissionToBeChecked, Set<Principal> principals) {
+        Subject subject = new Subject();
+        subject.getPrincipals().addAll(principals);
+
+        return implies(permissionToBeChecked, subject);
+    }
+
+    /**
+     * Optional method; TODO: needed?
+     */
+    default void refresh() { }
+
+}
---
+++ api/src/main/java/jakarta/security/jacc/PolicyFactory.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2023 Contributors to Eclipse Foundation. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+package jakarta.security.jacc;
+
+/**
+ * Abstract factory and finder class for obtaining the instance of the class that implements the
+ * PolicyFactory of a provider. The factory will be used to instantiate Policy objects.
+ *
+ * <p>
+ * <b>NOTE: DRAFT API. SUBJECT TO CHANGE</b>
+ *
+ * @see Policy
+ *
+ * @author Arjan Tijms
+ */
+public abstract class PolicyFactory {
+
+    public static final String FACTORY_NAME = "jakarta.security.jacc.PolicyFactory.provider";
+
+    private static volatile PolicyFactory policyFactory;
+
+    /**
+     * Get the system-wide PolicyFactory implementation.
+     *
+     * <p>
+     * The name of the factory implementation class is obtained from the
+     * value of the system property,
+     * <pre>{@code
+     *     jakarta.security.jacc.PolicyFactory.provider
+     * }
+     * </pre>
+     *
+     * This value can also be retrieved from the field {@code PolicyFactory.FACTORY_NAME}.
+     *
+     * @return the system-wide singleton instance of the provider specific PolicyFactory implementation class.
+     *
+     * @exception SecurityException If an exception was thrown during the class loading, or construction of the default
+     * PolicyFactory implementation class; in which case the SecurityException will contain the root Exception as its
+     * cause.
+     */
+    public static synchronized PolicyFactory getPolicyFactory() {
+        if (policyFactory != null) {
+            return policyFactory;
+        }
+
+        final String className = System.getProperty(FACTORY_NAME);
+        if (className != null) {
+            try {
+                policyFactory = (PolicyFactory)
+                    Class.forName(
+                            className,
+                            true,
+                            Thread.currentThread().getContextClassLoader())
+                         .getDeclaredConstructor()
+                         .newInstance();
+            } catch (ReflectiveOperationException pae) {
+                throw new SecurityException(pae);
+            }
+        }
+
+        return policyFactory;
+    }
+
+    /**
+     * Set the system-wide PolicyFactory implementation.
+     *
+     * <p>
+     * If an implementation was set previously, it will be replaced.
+     *
+     * @param policyFactory The PolicyFactory instance, which may be null.
+     *
+     */
+    public static synchronized void setPolicyFactory(PolicyFactory policyFactory) {
+        PolicyFactory.policyFactory = policyFactory;
+    }
+
+    /**
+     * This method is used to obtain an instance of the provider specific class that implements the {@link Policy}
+     * interface that corresponds to policy context within the provider. The policy context is identified by
+     * the value of the policy context identifier associated with the thread on which the accessor is called.
+     *
+     * <p>
+     * For a given determined value of the policy context identifier, this method must always return the same instance of
+     * {@link Policy} and there must be at most one actual instance of a {@link Policy} with a given policy
+     * context identifier (during a process context).
+     *
+     * <p>
+     * This method should be logically identical to calling {@link PolicyFactory#getPolicy(String)}
+     * with as input the value returned from {@link PolicyContext#getContextID()}.
+     *
+     * @return an Object that implements the {@link Policy} interface corresponding to the identified policy context,
+     * or a null if such an Object is not present.
+     */
+    public Policy getPolicy() {
+        return getPolicy(PolicyContext.getContextID());
+    }
+
+    /**
+     * Set the context Policy implementation.
+     *
+     * <p>
+     * If an implementation was set previously, it will be replaced.
+     *
+     * <p>
+     * This method should be logically identical to calling {@link PolicyFactory#setPolicy(String, Policy) }
+     * with as input for the first parameter the value returned from {@link PolicyContext#getContextID()}.
+     *
+     * @param policy The policy instance, which may be null.
+     *
+     */
+    public void setPolicy(Policy policy) {
+        setPolicy(PolicyContext.getContextID(), policy);
+    }
+
+    /**
+     * This method is used to obtain an instance of the provider specific class that implements the {@link Policy}
+     * interface that corresponds to policy context within the provider. The policy context is identified by
+     * the value of the policy context identifier associated with the thread on which the accessor is called.
+     *
+     * <p>
+     * For a given determined value of the policy context identifier, this method must always return the same instance of
+     * {@link Policy} and there must be at most one actual instance of a {@link Policy} with a given policy
+     * context identifier (during a process context).
+     *
+     * @param contextID A String identifying the policy context whose {@link Policy} interface is to be returned. The
+     * value passed to this parameter can be null, which corresponds to the system-wide default {@link Policy} instance.
+     *
+     *
+     * @return an Object that implements the {@link Policy} interface corresponding to the identified policy context,
+     * or a null if such an Object is not present.
+     */
+    public abstract Policy getPolicy(String contextId);
+
+    /**
+     * Set the context Policy implementation.
+     *
+     * <p>
+     * If an implementation was set previously, it will be replaced.
+     *
+     * @param contextID A String identifying the policy context whose {@link Policy} interface is to be returned. The
+     * value passed to this parameter can be null, which corresponds to the system-wide default {@link Policy} instance.
+     *
+     * @param policy The policy instance, which may be null.
+     *
+     */
+    public abstract void setPolicy(String contextId, Policy policy);
+
+}
